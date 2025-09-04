0876321eeefffca1d43e0fac43fe3fc665ce7cf9
Kevin <140569159+Kevgmd@users.noreply.github.com>
2025-09-04 00:43:33 +0000

    Script and stylesheet created

---
+++ JS/RandomQuote.js
@@ -0,0 +1,15 @@
+const images = [
+    'https://images.unsplash.com/photo-1756758006047-efc2f2b7d493?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    'https://images.unsplash.com/photo-1755429518361-1d6060edcf3c?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    'https://images.unsplash.com/photo-1731818754814-53dce99838d1?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    'https://images.unsplash.com/photo-1756747646179-d5652667914e?q=80&w=899&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    'https://images.unsplash.com/photo-1756049060197-37b4b52c3183?q=80&w=986&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+];
+
+const randomImage = images[Math.floor(Math.random() * images.length)];
+
+document.body.style.backgroundImage = `url(${randomImage})`;
+
+const quotes = [
+
+];
