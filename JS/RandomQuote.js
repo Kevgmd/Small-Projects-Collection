4a0d64c5c616c44c09253f4667b06d4a2aeb8409
Kevin <140569159+Kevgmd@users.noreply.github.com>
2025-09-12 01:21:48 +0000

    Quote text slightly enhanced

--- JS/RandomQuote.js
+++ JS/RandomQuote.js
@@ -1,16 +1,18 @@
 const quotes = [
     '"show bobs and vagenes pls" <br> - Mahatma Ghandi',
     '"Cada cachorro que lamba a sua própria caceta" <br> - Albert Einstein',
-    '"As coisa cai" <br> - Isaque Nilton'
+    '"As coisa cai" <br> - Isaque Nilton',
+    '"Casa vazia é oficina da punheta" <br> - Mario Ayala',
 ];
 
 const backgrounds = {
     [quotes[0]]: 'url("https://observatorio3setor.org.br/wp-content/uploads/2021/12/gandhi-portal.jpg")',
     [quotes[1]]: 'url("https://static.todamateria.com.br/upload/al/be/albert-einstein-og.jpg")',
     [quotes[2]]: 'url(https://lftm.com.br/wp-content/uploads/2024/05/Isaac-Newton.jpeg)',
-    [quotes[3]]: 'url(https://fabiobarros.com/wp-content/uploads/2020/07/img-dia-internacional-de-nelson-mandela.jpg)',
-    [quotes[4]]: 'url(https://s2-galileu.glbimg.com/lL94BrMvGp__xHE_9JaV8rMx1Qk=/0x0:1024x634/888x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_fde5cd494fb04473a83fa5fd57ad4542/internal_photos/bs/2025/x/6/RmpWFvTkCrjtlu3VQrNQ/51333949572-bf63a71465-b.jpg)',
-    [quotes[5]]: 'url(https://courier.unesco.org/sites/default/files/styles/best_image/article/courier/photos/nelson_mandela.jpg?itok=ydB_39uH)'
+    [quotes[3]]: 'url(https://img.observatoriodatv.com.br/wp-content/uploads/2022/05/Mario-Ayala-Gustavo-Daneluz-em-Carrossel-1024x768.jpg)',
+    [quotes[4]]: 'url(https://fabiobarros.com/wp-content/uploads/2020/07/img-dia-internacional-de-nelson-mandela.jpg)',
+    [quotes[5]]: 'url(https://courier.unesco.org/sites/default/files/styles/best_image/article/courier/photos/nelson_mandela.jpg?itok=ydB_39uH)',
+    [quotes[6]]: 'url(https://s2-galileu.glbimg.com/lL94BrMvGp__xHE_9JaV8rMx1Qk=/0x0:1024x634/888x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_fde5cd494fb04473a83fa5fd57ad4542/internal_photos/bs/2025/x/6/RmpWFvTkCrjtlu3VQrNQ/51333949572-bf63a71465-b.jpg)',
 };
 
 const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
