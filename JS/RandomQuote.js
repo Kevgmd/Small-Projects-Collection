58f58ef8173bfd8d49a5cb1d2f7110cb706fd0ed
Kevin <140569159+Kevgmd@users.noreply.github.com>
2025-09-13 15:19:37 +0000

    Improved Button

--- JS/RandomQuote.js
+++ JS/RandomQuote.js
@@ -3,6 +3,8 @@ const quotes = [
     '"Cada cachorro que lamba a sua própria caceta" <br> - Albert Einstein',
     '"As coisa cai" <br> - Isaque Nilton',
     '"Casa vazia é oficina da punheta" <br> - Mario Ayala',
+    '"Underidoderidudoridoderidoo" <br> - Winston Churchill',
+    '"Se o Tadala tá caro, empurra mole mesmo" <br> - Martin Luther King Jr.'
 ];
 
 const backgrounds = {
@@ -10,19 +12,17 @@ const backgrounds = {
     [quotes[1]]: 'url("https://static.todamateria.com.br/upload/al/be/albert-einstein-og.jpg")',
     [quotes[2]]: 'url(https://lftm.com.br/wp-content/uploads/2024/05/Isaac-Newton.jpeg)',
     [quotes[3]]: 'url(https://img.observatoriodatv.com.br/wp-content/uploads/2022/05/Mario-Ayala-Gustavo-Daneluz-em-Carrossel-1024x768.jpg)',
-    [quotes[4]]: 'url(https://fabiobarros.com/wp-content/uploads/2020/07/img-dia-internacional-de-nelson-mandela.jpg)',
-    [quotes[5]]: 'url(https://courier.unesco.org/sites/default/files/styles/best_image/article/courier/photos/nelson_mandela.jpg?itok=ydB_39uH)',
-    [quotes[6]]: 'url(https://s2-galileu.glbimg.com/lL94BrMvGp__xHE_9JaV8rMx1Qk=/0x0:1024x634/888x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_fde5cd494fb04473a83fa5fd57ad4542/internal_photos/bs/2025/x/6/RmpWFvTkCrjtlu3VQrNQ/51333949572-bf63a71465-b.jpg)',
+    [quotes[4]]: 'url(https://media.gazetadopovo.com.br/2025/05/31143751/churchill-1941-660x372.jpg.webp)',
+    [quotes[5]]: 'url(https://static.todamateria.com.br/upload/ma/rt/martinlutherking-cke.jpg)',
 };
 
-const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+let randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
 
 document.querySelector('h1').innerHTML = randomQuote;
 
 document.body.style.backgroundImage = backgrounds[randomQuote];
 
 const button = document.querySelector('button');
-
 button.addEventListener('click', function () {
     const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
     document.querySelector('h1').innerHTML = randomQuote;
