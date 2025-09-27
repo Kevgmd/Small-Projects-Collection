b0d85afd2edfdc6a7c0e61914d3ac5731f79b520
Kevin <140569159+Kevgmd@users.noreply.github.com>
2025-09-27 02:13:34 +0000

    CSS improved 2

--- JS/FahrenheitToCelcius.js
+++ JS/FahrenheitToCelcius.js
@@ -5,11 +5,16 @@ function convert() {
     const inputValue = document.querySelector('input').value;
 
     if (inputValue == '') {
-        insertMessage.textContent = 'Please insert a value';
-        finalValueDisplay.textContent = '';
+        insertMessage.style.visibility = 'visible';
+        insertMessage.style.opacity = '1';
+        finalValueDisplay.style.visibility = 'hidden';
+        finalValueDisplay.style.opacity = '0';
     } else {
         let celciusOutput = (inputValue - 32) * 5 / 9;
         finalValueDisplay.textContent = celciusOutput.toFixed(2) + '°C';
-        insertMessage.textContent = '';
+        insertMessage.style.visibility = 'hidden';
+        insertMessage.style.opacity = '0';
+        finalValueDisplay.style.visibility = 'visible';
+        finalValueDisplay.style.opacity = '1';
     }
 }
